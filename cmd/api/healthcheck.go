832bfc015ffcb2e101d5d884de317f28fcc8f242
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2021-06-28 10:21:49 +0200

    Encoding Structs

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -18,7 +18,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	// containing the encoded JSON. If there was an error, we log it and send the client
 	// a generic error message.
 	err := app.writeJSON(w, http.StatusOK, data, nil)
-
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -44,7 +44,6 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
 	// Encode the data to JSON, returning the error if there was one.
 	js, err := json.Marshal(data)
-
 	if err != nil {
 		return err
 	}
