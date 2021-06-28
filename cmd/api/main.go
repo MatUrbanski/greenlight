c3e6383dceda75b8ff921722d9871e0c1f30d878
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2021-06-28 13:39:22 +0200

    Setting up the Movie model

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"time"
 
+	"greenlight.mateuszurbanski.net/internal/data"
+
   // Import the pq driver so that it can register itself with the database/sql
   // package. Note that we alias this import to the blank identifier, to stop the Go
   // compiler complaining that the package isn't being used.
@@ -55,6 +57,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -104,6 +107,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	// Declare a HTTP server with some sensible timeout settings, which listens on the
